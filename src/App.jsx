bea76b8e16d4dd10fe83dc597831b38792b9a75f
Syl <darksideofmbs2020@gmail.com>
2025-02-09 00:08:06 +0630

    loginAndRegister

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const App = () => {
         {
           path : "/login",
           element : <Login />
+        },
+        {
+          path : "/register",
+          element : <Register />
         }
       ]
     }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,31 +31,34 @@ export default function Login() {
     }
 
     return (
-        <div className="mt-10 w-full max-w-lg mx-auto">
-            <form onSubmit={login} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <h1 className="text-2xl font-bold text-blue-500 text-center">Login Form</h1>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                        Email
-                    </label>
-                    <input value={email} onChange={e => setEmail(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Email" />
-                    {!!(error) && <p className="text-red-500 text-xs italic">{error}</p>}
+        <div className="w-full max-w-lg mx-auto">
+            <form onSubmit={login} class="mt-20 max-w-sm mx-auto">
+                <div class="mb-5">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                    <input value={email} onChange={e => setEmail(e.target.value)} type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required />
                 </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                        Password
-                    </label>
-                    <input value={password} onChange={e => setPassword(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                <div class="mb-5">
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                    <input value={password} onChange={e => setPassword(e.target.value)} type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="******************" required />
                 </div>
+                <div class="flex items-start mb-5">
+                    <div class="flex items-center h-5">
+                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                    </div>
+                    <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                </div>
+                
                 <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    <button className="bg-blue-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline" type="submit">
                         Login
                     </button>
-                    <Link to="/sign-up" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-500" href="#">
+                    <Link to="/register" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-500" href="#">
                         Register here
                     </Link>
                 </div>
+
             </form>
+
         </div>
     )
 }
