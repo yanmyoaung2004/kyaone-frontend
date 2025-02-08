04d6e8f17e089c218dacdb83c57548c2545efbdb
yanmyoaung2004 <ymyo44277@gmail.com>
2025-02-08 18:00:46 +0630

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Test from "./pages/Test";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/Test.jsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div className="text-red-400 text-5xl">Test</div>;
+};
+
+export default Test;
